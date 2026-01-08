c06029631c950eae06c6f40b77d6145a241cbe1d
shreyash sanjay gadling <shreyashgadling007@gmail.com>
2026-01-08 22:08:14 +0530

    Create question_problems.sql with content analysis queries
    Added SQL queries to analyze movie and TV show data.

---
+++ question_problems.sql
@@ -0,0 +1,16 @@
+-- 1. Count the number of Movies vs TV Shows
+-- 2. Find the most common rating for movies and TV shows
+-- 3. List all movies released in a specific year (e.g., 2020)
+-- 4. Find the top 5 countries with the most content on Netflix
+-- 5. Identify the longest movie
+-- 6. Find content added in the last 5 years
+-- 7. Find all the movies/TV shows by director 'Rajiv Chilaka'!
+-- 8. List all TV shows with more than 5 seasons
+-- 9.Find each year and the average numbers of content release in India on netflix. 
+-- 10. List all movies that are documentaries
+-- 11. Find all content without a director
+-- 12. Find how many movies actor 'Salman Khan' appeared in last 10 years!
+/* Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
+-- the description field. Label content containing these keywords as 'Bad' and all other 
+content as 'Good'. Count how many items fall into each category.
+*/
